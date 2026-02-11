0328b7a12148a5d9f4c383f52d207afb84d7f966
19Pashka91 <kobyakov_pavel@mail.ru>
2026-02-12 00:04:43 +0500

    Изменено форматирование

--- sandbox/src/main/java/ru/stqa/java_autotest/sanbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/java_autotest/sanbox/MyFirstProgram.java
@@ -1,10 +1,10 @@
 package ru.stqa.java_autotest.sanbox;
 
-public class MyFirstProgram{
+public class MyFirstProgram {
 
-public static void main (String args[]){
+  public static void main(String args[]) {
 
-System.out.println("Hello, World!");
+    System.out.println("Hello, World!");
 
-}
+  }
 }
